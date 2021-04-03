dedd802b76bc8c9851d7eedb52b90a032c2629cb
rishabhprakash1 <rishabhprakash01@gmail.com>
2021-04-04 00:39:17 +0530

    Initial commit

---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,32 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName){
+        Object restaurantName1 = restaurantName;
+        if (restaurants.contains(restaurantName1))
+            return (Restaurant) restaurantName1;
+        else return null;
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
